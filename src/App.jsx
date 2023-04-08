5dc8426e283fdf608147ee8d77e51a8e202a8e2c
Noman02 <noman.mahib001@gmail.com>
2023-04-08 16:14:50 +0600

    components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { BrowserRouter } from "react-router-dom"
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-    <div>3D Developer </div>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+        </div> 
+        <About/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+    </div>
+    </BrowserRouter>
   )
 }
 
