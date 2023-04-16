d9a7ff3ec75674f93c2825bba844f4dd5b549fdd
Noman02 <noman.mahib001@gmail.com>
2023-04-16 11:20:02 +0600

    initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,33 @@ import {Tilt} from "react-tilt";
 
 import { styles } from "../styles";
 import { services } from "../constants";
-
 import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard=({index,title,icon})=>{
+  return (
+    <Tilt className="xs:w-[280px] w-full">
+      <motion.div 
+      variants={fadeIn("right","spring", 0.5 * index, 0.75 )}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+        options={{
+          max:45,
+          scale:1,
+          speed:450
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col "
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h2 className='text-white text-[20px] font-bold text-center'>{title}</h2>
+
+        </div>
+
+      </motion.div>
+      
+    </Tilt>
+  )
+}
 
 const About = () => {
   return (
@@ -19,12 +44,23 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
+        I'm a skilled front-end developer with experience in Javascript and expertise in frameworks like React, I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        real-world problems. Let's work together to bring your ideas to FJK life!
       </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {
+          services.map((service,index)=> (
+            <ServiceCard
+             key={service.title}
+             index={index}
+             {...service} 
+            />
+          ))
+        }
+
+      </div>
    </>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,16 +46,12 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "React Developer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Front-End Developer",
       icon: backend,
-    },
-    {
-      title: "Content Creator",
-      icon: creator,
     },
   ];
   
