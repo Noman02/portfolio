9b9ada144bdd9cf5a08efeb9ae86e4fc858c22f6
Noman02 <noman.mahib001@gmail.com>
2023-04-20 08:40:05 +0600

    testimonials done

--- src/components/ReadMore.jsx
+++ src/components/ReadMore.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 
 function App() {
   const [readMore,setReadMore]=useState(false);
+
   const extraContent=<div>
       <p className="extra-content">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consectetur neque ab 
@@ -9,7 +10,10 @@ function App() {
         commodi officia aliquam! Maxime.
       </p>
   </div>
+
+  
   const linkName=readMore?'Read Less << ':'Read More >> '
+
   return (
     <div className="App">
       <a className="read-more-link" onClick={()=>{setReadMore(!readMore)}}><h2>{linkName}</h2></a>
